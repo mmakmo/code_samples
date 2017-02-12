9504d92b5f6c4ba565ba64111fe9e21cb30957de
Makoto Morita <mmakmo@gmail.com>
2017-02-12 19:48:29 +0900

    add option sample

--- bats_samples/bats_sample.sh
+++ bats_samples/bats_sample.sh
@@ -1,3 +1,42 @@
 #!/usr/bin/env bash
 
+errtrap() {
+  errcode=$?
+  echo "error line $1: command exited with status $errcode."
+}
+
+initialize() {
+  set -euo pipefail
+  declare TMP_DIR=$(mktemp -d)
+  trap "
+    errtrap $LINENO
+    rm -rf ${TMP_DIR}
+    echo 'completed with errors.'
+  " ERR
+}
+
+usage_exit() {
+  echo "Usage: $0 [-a] [-d dir] item ..." 1>&2
+  exit 1
+}
+
+initialize
+
+while getopts ad:h OPT
+do
+  case $OPT in
+    a)  FLAG_A=1
+        ;;
+    d)  VALUE_D=$OPTARG
+        ;;
+    h)  usage_exit
+        ;;
+    \?) usage_exit
+        ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+
 echo "hello world"
+
--- bats_samples/test-bats_sample.bats
+++ bats_samples/test-bats_sample.bats
@@ -1,7 +1,8 @@
 #!/usr/bin/env bats
 
-# setup() {
-#}
+setup() {
+  rm -rf /tmp/tmp.*/
+}
 
 #teardown() {
 #}
@@ -10,3 +11,17 @@
   run ./bats_sample.sh
   [[ ${lines[0]} = "hello world" ]]
 }
+
+@test "deleted tempolary directory at the end of process" {
+  run ./bats_sample.sh
+  run ls /tmp/tmp.*
+  [ "$status" -ne 2 ]
+  # [ "$output" = "ls: cannot access '/tmp/tmp.*': No such file or directory" ]
+}
+
+@test "shows illigal option error" {
+  run ./bats_sample.sh -z
+  [[ ${lines[0]} = "./bats_sample.sh: illegal option -- z" ]]
+  [[ ${lines[1]} = "Usage: ./bats_sample.sh [-a] [-d dir] item ..." ]]
+}
+
